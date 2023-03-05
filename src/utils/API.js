a0bc98b68fa8be8ee0bebf436ac7e08d3743be71
Chiahsuan Chou <belinda05021992@gmail.com>
2023-03-06 07:31:03 +0800

    add API utils

---
+++ src/utils/API.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_APP_FRONTEND_HOST,
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json'
+  }
+});
+
+API.interceptors.request.use(
+  config => {
+    const token = Cookies.get('PlatingToken');
+    if (token) config.headers['Authorization'] = `Bearer ${token}`;
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+  // 2xx
+  response => {
+    return {
+      data: response.data,
+      meta: response.data.meta
+    };
+  }
+);
+
+export default {
+  async GET(path, params) {
+    const response = await API.get(path, params);
+    return response;
+  },
+  async POST(path, form) {
+    const response = await API.post(path, form);
+    return response;
+  },
+  async PUT(path, form, params) {
+    const response = await API.put(path, form, params);
+    return response;
+  },
+  async PATCH(path, form) {
+    const response = await API.patch(path, form);
+    return response;
+  },
+  async DELETE(path, params) {
+    const response = await API.delete(path, params);
+    return response;
+  }
+};
