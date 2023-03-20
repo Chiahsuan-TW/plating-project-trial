3ec244195c1dd0613d83e387206d69492abc2dcf
Jade <y3m45566@gmail.com>
2023-03-20 17:51:10 +0800

    [feat] Uploader component

---
+++ src/components/Uploader.vue
@@ -0,0 +1,155 @@
+<script setup>
+import { ref, computed } from 'vue';
+import API from '@/utils/API.js';
+import { UploadOutlined } from '@ant-design/icons-vue';
+
+const props = defineProps({
+  isWorksheetComplete: {
+    type: Boolean,
+    require: true
+  },
+  fileList: {
+    type: Array,
+    require: true,
+    default: () => []
+  }
+});
+
+const fileList = ref(props.fileList);
+
+// const emit = defineEmits({
+//     updateFileList: fileList => {
+//       if (fileList.length >= 2) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     }
+// });
+
+const emit = defineEmits(['updateFileList']);
+
+// disable after worksheet was checked.
+const isDisable = computed(() => {
+  if (props.isWorksheetComplete) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+// trashcan button css control
+const trashcanDisplay = computed(() => {
+  if (isDisable.value) {
+    return 'none';
+  } else {
+    return 'block';
+  }
+});
+
+function emitFileList() {
+  emit('updateFileList', fileList.value);
+}
+
+/**
+ * handleChange 將圖片上傳 S3
+ * @param info:object: {
+ *    file: object: {
+ *      lastModified: number,
+ *      name: string,
+ *      size: number,
+ *      type: string,
+ *      uid: string,
+ *      webkitRelativePath: string
+      },
+     fileList: object[]
+  }
+ * @returns: void
+ */
+
+const handleChange = async info => {
+  if (fileList.value.length >= 6) {
+    alert('照片只能選擇最多六張');
+  } else {
+    const filename = info.file.name;
+    /**
+     * finalResponse: object: { filename: string, presignedURL: string }
+     */
+
+    try {
+      //   await API.UploadImg('photo', info.file);
+
+      //   const {
+      //     data: { data: finalResponse }
+      //   } = await API.POST('/share/get-presignedURL', { filename });
+
+      //   // put presigned-url add to fileList
+      //   // add backend return file name.
+      //   fileList.value.forEach((eachFile, index, array) => {
+      //     if (eachFile.name === filename) {
+      //       array[index].filename = finalResponse.filename;
+      //     }
+      //   });
+
+      emitFileList('updateFileList');
+    } catch (error) {
+      console.log('error', error.message);
+    }
+
+    // 練習
+    /**
+     *  getUploadUrlAndPhotoUrl: {
+     *      'upload_url': string;
+     *      'get_url': string;
+     *  }
+     */
+    // const { data: getUploadUrlAndPhotoUrl } = await axios.post(
+    //   'https://grndbc9554.execute-api.us-east-1.amazonaws.com/dev/jade-upload-s3',
+    //   {
+    //     fileName: filename,
+    //     fileType: 'image/jpeg'
+    //   }
+    // );
+
+    // await axios.put(getUploadUrlAndPhotoUrl['upload_url'], file);
+
+    // fileImgUrl.value = getUploadUrlAndPhotoUrl['get_url'];
+  }
+};
+
+// avoid show upload progress
+function beforeUpload(file) {
+  const isJPG = file.type === 'image/jpeg';
+  if (!isJPG) {
+    alert('You can only upload JPG file!');
+  }
+  return false;
+}
+</script>
+
+<template>
+  <div>
+    <AUpload
+      v-model:file-list="fileList"
+      class="upload-list-inline"
+      name="file"
+      listType="picture"
+      capture
+      :beforeUpload="beforeUpload"
+      @change="handleChange"
+      :disabled="isDisable"
+    >
+      <AButton :disabled="isDisable">
+        <UploadOutlined></UploadOutlined>
+        Click to upload
+      </AButton>
+    </AUpload>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep
+  button.ant-btn.ant-btn-text.ant-btn-sm.ant-upload-list-item-card-actions-btn.ant-btn-icon-only {
+  display: v-bind(trashcanDisplay);
+}
+</style>
--- src/views/TrelloView.vue
+++ src/views/TrelloView.vue
@@ -8,7 +8,9 @@ const controlOnStart = ref(true);
 // mounted get localStorage
 onMounted(() => {
   localList.value = localStorage.getItem('list1');
-  list1.value = [...JSON.parse(localList.value)];
+  // console.log('localList.value ', localList.value);
+
+  // list1.value = [...JSON.parse(localList.value)];
 });
 
 const list1 = ref([
@@ -41,6 +43,70 @@ const list1 = ref([
     id: 3,
     name: '太平所',
     workCard: [{ id: 0, name: '太平所 鍍膜1' }]
+  },
+  {
+    id: 4,
+    name: '深坑所',
+    workCard: [
+      { id: 0, name: '深坑所 鍍膜1' },
+      { id: 1, name: '深坑所 鍍膜2' }
+    ]
+  },
+  {
+    id: 5,
+    name: '新北所',
+    workCard: [
+      { id: 0, name: '新北所 鍍膜1' },
+      { id: 1, name: '新北所 鍍膜2' }
+    ]
+  },
+  {
+    id: 6,
+    name: '景美所',
+    workCard: [
+      { id: 0, name: '景美所 鍍膜1' },
+      { id: 1, name: '景美所 鍍膜2' }
+    ]
+  },
+  {
+    id: 7,
+    name: '新店所',
+    workCard: [
+      { id: 0, name: '新店所 鍍膜1' },
+      { id: 1, name: '新店所 鍍膜2' }
+    ]
+  },
+  {
+    id: 8,
+    name: '北安所',
+    workCard: [
+      { id: 0, name: '北安所 鍍膜1' },
+      { id: 1, name: '北安所 鍍膜2' }
+    ]
+  },
+  {
+    id: 9,
+    name: '小碧潭所',
+    workCard: [
+      { id: 0, name: '小碧潭所 鍍膜1' },
+      { id: 1, name: '小碧潭所 鍍膜2' }
+    ]
+  },
+  {
+    id: 10,
+    name: '信義所',
+    workCard: [
+      { id: 0, name: '信義所 鍍膜1' },
+      { id: 1, name: '信義所 鍍膜2' }
+    ]
+  },
+  {
+    id: 11,
+    name: '大直所',
+    workCard: [
+      { id: 0, name: '大直所 鍍膜1' },
+      { id: 1, name: '大直所 鍍膜2' }
+    ]
   }
 ]);
 
@@ -101,4 +167,4 @@ function putAPI() {
   </div>
 </template>
 
-<style></style>
+<style scoped></style>
